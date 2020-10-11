5f2a1fb575ab82027df71349c3b75366f78f663b
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2020-10-12 00:26:09 +0300

    3 task
    3 task

--- Task5/src/main/java/by/training/task5/service/SubstringByLengthChanger.java
+++ Task5/src/main/java/by/training/task5/service/SubstringByLengthChanger.java
@@ -2,6 +2,6 @@ package by.training.task5.service;
 
 public class SubstringByLengthChanger {
     public String changeSubstringByLength(String stringToChange, String substring, int length) {
-        return stringToChange.replaceAll("\\b[a-zA-Z]{" + length + "}\\b", substring);
+        return stringToChange.replaceAll("\\b[a-zA-Z-А-Яа-я]{" + length + "}\\b", substring);
     }
 }
