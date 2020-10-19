aacd84fdb5692cf472d1c9ad67262595dd158008
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2020-10-19 20:33:28 +0300

    task 6 init
    task 6 init

---
+++ Task6/src/main/java/by/training/entity/Author.java
@@ -0,0 +1,5 @@
+package by.training.entity;
+
+public class Author {
+
+}
---
+++ Task6/src/main/java/by/training/entity/Book.java
@@ -0,0 +1,12 @@
+package by.training.entity;
+
+import java.util.List;
+
+public class Book {
+    private String title;
+    private List<Author> authors;
+    private int numberOfPages;
+    private String publishingHouse;
+    private int yearOfPublishing;
+
+}
