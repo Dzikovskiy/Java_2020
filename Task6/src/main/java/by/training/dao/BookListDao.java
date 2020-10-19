69f0129d2c63e9cfbe8f687f7b4c1fdc51a1e0f8
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2020-10-19 21:39:56 +0300

    author
    author

---
+++ Task6/src/main/java/by/training/dao/BookListDao.java
@@ -0,0 +1,16 @@
+package by.training.dao;
+
+import by.training.entity.Book;
+
+public class BookListDao {
+    public void addBook(Book book) {
+
+    }
+
+    public void removeBook(Book book) {
+
+    }
+
+    public void 
+
+}
