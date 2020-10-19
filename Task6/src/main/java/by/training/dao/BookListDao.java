d7701402a43713b1e6f6a17e29a16defbf067c69
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2020-10-19 23:59:01 +0300

    dao book
    dao book

--- Task6/src/main/java/by/training/dao/BookListDao.java
+++ Task6/src/main/java/by/training/dao/BookListDao.java
@@ -2,7 +2,13 @@ package by.training.dao;
 
 import by.training.entity.Book;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class BookListDao {
+    private ArrayList<Book> books = new ArrayList<>();
+
     public void addBook(Book book) {
 
     }
@@ -11,6 +17,16 @@ public class BookListDao {
 
     }
 
-    public void 
+    public ArrayList<Book> findByTag(ISpecification<Book> specification){
+        Stream<Book> result = books.stream();
+
+        if(specification.getFilter()!=null){
+            result= books.stream().filter(specification.getFilter());
+        }
+        if(specification.getSorted()!=null){
+            result = books.stream().sorted(specification.getSorted());
+        }
+        return (ArrayList<Book>) result.collect(Collectors.toList());
+    }
 
 }
