b4058ce68dc4083e41796ff1f66b394bce971541
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2020-10-21 19:04:36 +0300

    csv writer
    csv writer

--- Task6/src/main/java/by/training/Main.java
+++ Task6/src/main/java/by/training/Main.java
@@ -3,6 +3,7 @@ package by.training;
 import by.training.entity.Author;
 import by.training.entity.Book;
 import by.training.repository.BookRepository;
+import by.training.service.CSVWriter;
 
 import java.util.ArrayList;
 
@@ -11,22 +12,25 @@ public class Main {
         BookRepository bookRepository = new BookRepository();
 
         ArrayList<Book> books = bookRepository.getAll();
+//
+//        Book book = new Book();
+//        book.setIsbnNumber(123123123123L);
+//        book.setTitle("AHobbit");
+//        book.setNumberOfPages(1263);
+//        book.setPublishingHouse("Popury");
+//        book.setYearOfPublishing(1976);
+//        Author a1= new Author();
+//        Author a2 = new Author();
+//        a1.setName("Tolkin");
+//        a2.setName("jr Tolkin");
+//        book.addAuthor(a1);
+//        book.addAuthor(a2);
+//
+//        bookRepository.delete(book);
+//        books.forEach(System.out::println);
 
-        Book book = new Book();
-        book.setIsbnNumber(123123123123L);
-        book.setTitle("AHobbit");
-        book.setNumberOfPages(1263);
-        book.setPublishingHouse("Popury");
-        book.setYearOfPublishing(1976);
-        Author a1= new Author();
-        Author a2 = new Author();
-        a1.setName("Tolkin");
-        a2.setName("jr Tolkin");
-        book.addAuthor(a1);
-        book.addAuthor(a2);
-
-        bookRepository.delete(book);
-        books.forEach(System.out::println);
+        CSVWriter writer = new CSVWriter();
+        writer.writeBookListToFile(books);
 
     }
 }
