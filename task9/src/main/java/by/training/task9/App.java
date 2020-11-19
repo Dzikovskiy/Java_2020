9b0a84fa5102a6d0c232a532cf3faf779ea52fde
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2020-11-19 19:28:39 +0300

    task9 init
    task9 init

---
+++ task9/src/main/java/by/training/task9/App.java
@@ -0,0 +1,11 @@
+package by.training.task9;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+    public static void main(String[] args) {
+        Logger logger = LogManager.getLogger();
+        logger.info("Program start");
+    }
+}
