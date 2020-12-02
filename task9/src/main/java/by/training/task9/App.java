36d4703b7f322fca29f92bb8a42d22025000d70b
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2020-12-02 19:34:03 +0300

    task 2.4 init
    task 2.4 init

--- task9/src/main/java/by/training/task9/App.java
+++ task9/src/main/java/by/training/task9/App.java
@@ -23,20 +23,26 @@ public class App {
 
         parser.setCompoundCharacterComparator(new ListSizeComparator());
 
+        lexParser.setCompoundCharacterComparator(new ListSizeComparator().reversed());
+
 
         CompoundCharacter compoundCharacter = new CompoundCharacter();
         compoundCharacter.setCharacters("    It has survived not only five centuries, but also the leap into electronic\n" +
                 "typesetting, remaining essentially unchanged? It was popularised in the with the\n" +
-                "release of Letraset sheets containing Lorem. Ipsum passages, and more recently with\n" +
+                "release of Letraset sheets containing Lorem Ipsum passages, and more recently with\n" +
                 "desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
-                "\tIt. Is a established fact that a reader will be of a page when looking at its\n" +
+                "\tIt is a established fact that a reader will be of a page when looking at its\n" +
                 "layout.\n" +
                 "\tBye.\n" +
                 "    It is a long established fact that a reader will be distracted by the readable\n" +
                 "content of a page when looking at its layout. The point of using Ipsum is that it has a\n" +
                 "more-or-less normal distribution of letters, as opposed to using 'Content here, content\n" +
                 "here', making it look like readable English.\n" +
-                "    Bye. Bye. Bye. Bye. Bye. Bye. Bye. Bye.");
+                "    Bye. Bye. Bye. Bye. Bye. Bye. Bye. Bye.\n" +
+                "    Hello hell hel he h.\n" +
+                "    Hello hell hel he h.\n" +
+                "    Hello hell hel he h.\n" +
+                "    Hello hell hel he h.");
         ArrayList<CompoundCharacter> list = parser.parseManager(compoundCharacter);
 
     //    list = (ArrayList<CompoundCharacter>) list.stream().sorted(new ListSizeComparator().reversed()).collect(Collectors.toList());
